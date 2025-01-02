aa5056c5aace64169a2fad14bc557f222675ebc1
Brainiac Rawkib <brainiacrawkib@gmail.com>
2025-01-02 17:40:16 +0100

    updates

--- models/cashierModels.go
+++ models/cashierModels.go
@@ -3,7 +3,7 @@ package models
 import "time"
 
 type Cashier struct {
-	Id          uint      `json:"id"`
+	Id          uint      `json:"Id"`
 	Name        string    `json:"name"`
 	Password    string    `json:"password"`
 	DateCreated time.Time `json:"dateCreated"`
@@ -11,14 +11,14 @@ type Cashier struct {
 }
 
 type Category struct {
-	Id          int       `json:"id" gorm:"type:INT(10) UNSIGNED NOT NULL AUTO_INCREMENT;primaryKey"`
+	Id          int       `json:"Id" gorm:"type:INT(10) UNSIGNED NOT NULL AUTO_INCREMENT;primaryKey"`
 	Name        string    `json:"name"`
 	DateCreated time.Time `json:"dateCreated"`
 	DateUpdated time.Time `json:"dateUpdated"`
 }
 
 type Discount struct {
-	Id              int       `json:"id" gorm:"type:INT(10) UNSIGNED NOT NULL AUTO_INCREMENT;primaryKey"`
+	Id              int       `json:"Id" gorm:"type:INT(10) UNSIGNED NOT NULL AUTO_INCREMENT;primaryKey"`
 	Qty             int       `json:"qty"`
 	Type            string    `json:"type"`
 	Result          int       `json:"result"`
@@ -30,7 +30,7 @@ type Discount struct {
 }
 
 type Order struct {
-	Id             int       `json:"id" gorm:"type:INT(10) UNSIGNED NOT NULL AUTO_INCREMENT;primaryKey"`
+	Id             int       `json:"Id" gorm:"type:INT(10) UNSIGNED NOT NULL AUTO_INCREMENT;primaryKey"`
 	CashierId      int       `json:"cashierId"`
 	PaymentTypesId int       `json:"paymentTypesId"`
 	TotalPrice     int       `json:"totalPrice"`
@@ -42,4 +42,39 @@ type Order struct {
 	Quantities     string    `json:"quantities"`
 	DateCreated    time.Time `json:"dateCreated"`
 	DateUpdated    time.Time `json:"dateUpdated"`
+}
+
+type ProductResponseOrder struct {
+	ProductId        int      `json:"productId" gorm:"type:INT(10) UNSIGNED NOT NULL AUTO_INCREMENT;primaryKey"`
+	Name             string   `json:"name"`
+	Price            int      `json:"price"`
+	Qty              int      `json:"qty"`
+	Discount         Discount `json:"discount"`
+	TotalNormalPrice int      `json:"totalNormalPrice"`
+	TotalFinalPrice  int      `json:"totalFinalPrice"`
+}
+
+type ProductOrder struct {
+	Id         int    `json:"Id" gorm:"type:INT(10) UNSIGNED NOT NULL AUTO_INCREMENT;primaryKey"`
+	Sku        string `json:"sku"`
+	Name       string `json:"name"`
+	Stock      int    `json:"stock"`
+	Price      int    `json:"price"`
+	Image      string `json:"image"`
+	CategoryId int    `json:"categoryId"`
+	DiscountId int    `json:"discountId"`
+}
+
+type RevenueResponse struct {
+	PaymentTypeId int    `json:"paymentTypeId"`
+	Name          string `json:"name"`
+	Logo          string `json:"logo"`
+	TotalAmount   int    `json:"totalAmount"`
+}
+
+type SoldResponse struct {
+	ProductId   int    `json:"productId"`
+	Name        string `json:"name"`
+	TotalQty    int    `json:"totalQty"`
+	TotalAmount int    `json:"totalAmount"`
 }
