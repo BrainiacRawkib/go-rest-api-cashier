34dfd5fa82be188b28834d3f008c397d52ce82a1
Brainiac Rawkib <brainiacrawkib@gmail.com>
2025-01-02 17:46:46 +0100

    updates

--- models/payments.go
+++ models/payments.go
@@ -1,1 +1,13 @@
 package models
+
+import "time"
+
+type Payment struct {
+	Id            uint      `json:"id" gorm:"type:INT(10) UNSIGNED NOT NULL AUTO_INCREMENT;primaryKey"`
+	Name          string    `json:"name"`
+	Type          string    `json:"type"`
+	PaymentTypeId int       `json:"paymentTypeId"`
+	Logo          string    `json:"logo"`
+	DateCreated   time.Time `json:"dateCreated"`
+	DateUpdated   time.Time `json:"dateUpdated"`
+}
