49f98413ca7ed12a25e3e57c761825a6f1884f89
Brainiac Rawkib <brainiacrawkib@gmail.com>
2025-01-02 13:46:14 +0100

    updates

--- models/cashierModels.go
+++ models/cashierModels.go
@@ -11,8 +11,20 @@ type Cashier struct {
 }
 
 type Category struct {
-	Id          int       `json:"id"`
+	Id          int       `json:"id" gorm:"type:INT(10) UNSIGNED NOT NULL AUTO_INCREMENT;primaryKey"`
 	Name        string    `json:"name"`
 	DateCreated time.Time `json:"dateCreated"`
 	DateUpdated time.Time `json:"dateUpdated"`
+}
+
+type Discount struct {
+	Id              int       `json:"id" gorm:"type:INT(10) UNSIGNED NOT NULL AUTO_INCREMENT;primaryKey"`
+	Qty             int       `json:"qty"`
+	Type            string    `json:"type"`
+	Result          int       `json:"result"`
+	ExpiredAt       int       `json:"expiredAt"`
+	ExpiredAtFormat string    `json:"expiredAtFormat"`
+	StringFormat    string    `json:"stringFormat"`
+	DateCreated     time.Time `json:"dateCreated"`
+	DateUpdated     time.Time `json:"dateUpdated"`
 }
