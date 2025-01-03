7aa280a97e97b9806973c477ffe4104a3835873c
Brainiac Rawkib <brainiacrawkib@gmail.com>
2025-01-03 23:54:32 +0100

    updates

--- main.go
+++ main.go
@@ -1,21 +1,45 @@
 package main
 
 import (
-	"github.com/gofiber/fiber/v2"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
+func loadMySqlDBCreds() {
+	// Initialize .env vars
+	dbName := os.Getenv("MYSQL_DBNAME")
+	dbUser := os.Getenv("MYSQL_USER")
+	dbPassword := os.Getenv("MYSQL_PASSWORD")
+	dbHost := os.Getenv("MYSQL_HOST")
+	dbPort := os.Getenv("MYSQL_PORT")
+
+	fmt.Println("dbName: ", dbName)
+	fmt.Println("dbUser: ", dbUser)
+	fmt.Println("dbPassword: ", dbPassword)
+	fmt.Println("dbHost: ", dbHost)
+	fmt.Println("dbPort: ", dbPort)
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
+	fmt.Println("dsn: ", dsn)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("DB connection failed!")
+	}
+
+	DB = db
+	fmt.Println("DB connection successful!")
+}
+
 func main() {
-	// Instantiate Fiber
-	app := fiber.New()
+	// Load .env
+	godotenv.Load()
 
-	// http handler
-	app.Get("/testAPi", func(c *fiber.Ctx) error {
-		return c.Status(200).JSON(fiber.Map{
-			"success": true,
-			"message": "Fiber first application!",
-		})
-	})
-
-	// Run server
-	app.Listen(":4433")
+	// Load MySQL DB details
+	loadMySqlDBCreds()
 }
